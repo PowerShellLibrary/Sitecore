3bd72c11d0eddcb72379da4d2e8b1511d84d5af2
Alan Płócieniak <alan-null@outlook.com>
2019-09-13 17:00:10 +0200

    Open-Log.ps1

---
+++ Development/Open-Log.ps1
@@ -0,0 +1,77 @@
+<#
+
+.SYNOPSIS
+Helper script for removing inactive logs and opening the active one.
+
+.DESCRIPTION
+This script will do two things:
+- it will remove all inactive logs for a given location.
+- it will open the last active log using logview.exe
+
+You can pick which log should be opened:
+- Sitecore
+- Search
+- SPE
+- Publishing
+
+If you wish to work using single logview window use `SingleWindow` switch.
+
+.PARAMETER LogType
+Type of the log. It can be Sitecore log, Publishing, SPE etc.
+
+.PARAMETER LogsFolder
+Location of the folder where your Sitecore logs live.
+
+.PARAMETER SingleWindow
+Allow you to focus on a single logview window. When used previously opened logview windows will be closed.
+
+.EXAMPLE
+.\Open-Log.ps1 "c:\sitecore\Data\logs" -LogType Sitecore
+Removes all logs in "c:\sitecore\Data\logs\" and opens active Sitecore log
+
+.EXAMPLE
+.\Open-Log.ps1 "c:\sitecore\Data\logs" -LogType SPE
+Removes all logs in "c:\sitecore\Data\logs\" and opens active SPE log
+
+.EXAMPLE
+.\Open-Log.ps1 "c:\sitecore\Data\logs" -LogType SPE -SingleWindow
+Removes all logs in "c:\sitecore\Data\logs\" then closes all existing windows with logs and opens active Sitecore log
+
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$LogsFolder,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Sitecore", "Search", "SPE", "Publishing")]
+    [string]$LogType = 'Sitecore',
+    [Parameter(Mandatory = $false)]
+    [switch]$SingleWindow
+)
+
+process {
+    if (!(Test-Path $LogsFolder)) {
+        Write-Host "Incorrect logs folder path:" -ForegroundColor Red
+        Write-Host "$LogsFolder" -ForegroundColor Red
+        exit
+    }
+
+    if ($SingleWindow) {
+        Get-Process | ? { $_.Name -eq "logview" } | Stop-Process
+    }
+
+    Switch ($LogType) {
+        "Sitecore" { $ending = "log." }
+        "Search" { $ending = "Search.log" }
+        "SPE" { $ending = "SPE." }
+        "Publishing" { $ending = "Publishing." }
+    }
+
+    $currentFolder = Get-Item $PSScriptRoot
+    $exe = Join-Path $currentFolder.Parent.FullName "bin\logview.exe"
+
+    Get-ChildItem $LogsFolder | Remove-Item -ErrorAction SilentlyContinue | Out-Null
+    $firstLog = Get-ChildItem $LogsFolder | ? { $_.Name.StartsWith($ending) }
+    & "$exe" $firstLog.FullName
+}

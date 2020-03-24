8ffa734ff65c2f67466e839291ea2043ab1ddc09
Alan Płócieniak <alan-null@outlook.com>
2020-03-25 00:30:04 +0100

    Test-DLLs Run script in a separate process to prevent files from locking

--- Development/Test-DLLs.ps1
+++ Development/Test-DLLs.ps1
@@ -30,10 +30,6 @@ param(
     [string]$DllFolder
 )
 
-begin {
-    Import-Module DLLInfo
-}
-
 process {
     if (!(Test-Path $DllFolder)) {
         Write-Host "Incorrect folder path:" -ForegroundColor Red
@@ -41,22 +37,41 @@ process {
         exit
     }
 
-    Get-ChildItem $DllFolder -Filter "*.dll" | % {
-        Write-Host $_.FullName -ForegroundColor Yellow
-        $versionInfo = (Get-Item $_.FullName).VersionInfo
-        Write-Host "FileVersion   `t`t[$($versionInfo.FileVersion)]"
-        Write-Host "ProductVersion`t`t[$($versionInfo.ProductVersion)]"
-        Write-Host "LegalCopyright`t`t[$($versionInfo.LegalCopyright)]"
-        $buildConfiguration = Get-BuildConfiguration $_.FullName
-        if ($buildConfiguration -eq "Release") {
-            Write-Host "BuildConfiguration`t[$buildConfiguration]" -ForegroundColor Green
-        }else {
-            Write-Host "BuildConfiguration`t[$buildConfiguration]" -ForegroundColor Red
+    $scriptBlock = {
+        [CmdletBinding()]
+        param([string]$DllFolder)
+
+        begin {
+            Import-Module DLLInfo
         }
-        $targetCPU = Get-TargetCPU $_.FullName
-        Write-Host "TargetCPU`t`t[$targetCPU] "
-        $jitOptimized = Test-JitOptimized $_.FullName
-        Write-Host "JitOptimized`t`t[$jitOptimized]"
-        write-host ""
+
+        process {
+            Get-ChildItem $DllFolder -Filter "*.dll" | % {
+                Write-Host $_.FullName -ForegroundColor Yellow
+                $versionInfo = (Get-Item $_.FullName).VersionInfo
+                Write-Host "FileVersion   `t`t[$($versionInfo.FileVersion)]"
+                Write-Host "ProductVersion`t`t[$($versionInfo.ProductVersion)]"
+                Write-Host "LegalCopyright`t`t[$($versionInfo.LegalCopyright)]"
+                $buildConfiguration = Get-BuildConfiguration $_.FullName
+                if ($buildConfiguration -eq "Release") {
+                    Write-Host "BuildConfiguration`t[$buildConfiguration]" -ForegroundColor Green
+                }
+                else {
+                    Write-Host "BuildConfiguration`t[$buildConfiguration]" -ForegroundColor Red
+                }
+                $targetCPU = Get-TargetCPU $_.FullName
+                Write-Host "TargetCPU`t`t[$targetCPU] "
+                $jitOptimized = Test-JitOptimized $_.FullName
+                Write-Host "JitOptimized`t`t[$jitOptimized]"
+                write-host ""
+            }
+        }
     }
+
+    $tmp = New-TemporaryFile
+    $scriptBlock.ToString() | Out-File $tmp.FullName
+    $scriptFilePath = [System.IO.Path]::ChangeExtension($tmp.FullName, ".ps1")
+    $tmp | Rename-Item -NewName $tmp.Name.Replace(".tmp", ".ps1")
+    Invoke-Expression "powershell.exe $scriptFilePath $DllFolder"
+    Remove-Item $scriptFilePath -Force
 }
